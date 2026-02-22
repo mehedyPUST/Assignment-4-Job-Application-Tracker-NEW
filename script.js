a11b5690a34ade83a7cc28b84f7b118e4c7ad506
mehedyPUST <mehedy.pust@gmail.com>
2026-02-22 19:10:16 +0600

    interview filtering done

--- script.js
+++ script.js
@@ -41,51 +41,78 @@ function toggleBtnStyle(id) {
     clicked.classList.remove('bg-gray-200', 'text-gray-800');
     clicked.classList.add('bg-blue-500', 'text-white');
 
+    if (id == 'btn-interview-filter') {
+        allJobsCard.classList.add('hidden');
+        filteredJobList.classList.remove('hidden');
+
+    } else if (id == 'btn-all-filter') {
+        allJobsCard.classList.remove('hidden');
+        filteredJobList.classList.add('hidden');
+    }
+
 
 }
 
 mainContainer.addEventListener('click', function (event) {
+
+
+    console.log(event.target.parentNode.parentNode);
+    console.log(event.target.classList.contains('btn-interview'));
+
+    if (event.target.classList.contains('btn-interview')) {
+        const parentNode = event.target.parentNode.parentNode;
+        const jobProvider = parentNode.querySelector('.job-provider').innerText;
+        const jobTitle = parentNode.querySelector('.job-title').innerText;
+        const locationTypeSalary = parentNode.querySelector('.location-type-salary').innerText;
+        const jobStatus = parentNode.querySelector('.job-status').innerText;
+        const jobDetails = parentNode.querySelector('.job-details').innerText;
+
+
+        const cardInfo = {
+            jobProvider,
+            jobTitle,
+            locationTypeSalary,
+            jobStatus,
+            jobDetails
+        };
+
+        const jobProviderExist = interviewList.find(item => item.jobProvider == cardInfo.jobProvider);
+        parentNode.querySelector('.job-status').innerHTML = `<p
+                            class=" btn-interview  py-2 px-4 w-30 rounded-sm font-bold  text-[#10B981] border-2 border-[#10B981]">INTERVIEW</p>
+`;
+        if (!jobProviderExist) {
+            interviewList.push(cardInfo);
+        }
+        renderInterview();
+    }
+
+})
+
+function renderInterview() {
+    filteredJobList.innerHTML = '';
+
     const parentNode = event.target.parentNode.parentNode;
-    const jobProvider = parentNode.querySelector('.job-provider');
+    const jobProvider = parentNode.querySelector('.job-provider').innerText;
     const jobTitle = parentNode.querySelector('.job-title').innerText;
     const locationTypeSalary = parentNode.querySelector('.location-type-salary').innerText;
     const jobStatus = parentNode.querySelector('.job-status').innerText;
     const jobDetails = parentNode.querySelector('.job-details').innerText;
 
 
-    const cardInfo = {
-        jobProvider,
-        jobTitle,
-        locationTypeSalary,
-        jobStatus,
-        jobDetails
-    };
-
-    const jobProviderExist = interviewList.find(item => item.jobProvider == cardInfo.jobProvider);
-    if (!jobProviderExist) {
-        interviewList.push(cardInfo);
-    }
-    console.log(interviewList);
-
-})
-
-function renderInterview() {
-    filteredJobList.innerHTML = '';
     for (let interview of interviewList) {
+        console.log(interview);
         let div = document.createElement('div');
         div.className = 'flex justify-between p-4 border border-blue-200 bg-gray-50 rounded-sm';
         div.innerHTML = `
         <div class="space-y-2">
-                    <h2 class="job-provider text-[18px] font-semibold">Mobile First Corp</h2>
-                    <p class="job-title text-[#64748B]">React Native Developer</p>
+                    <h2 class="job-provider text-[18px] font-semibold">${interview.jobProvider}</h2>
+                    <p class="job-title text-[#64748B]">${jobTitle}</p>
                     <br>
-                    <p class="location-type-salary text-[#64748B]">Remote • Full-time • $130,000 - $175,000</p>
+                    <p class="location-type-salary text-[#64748B]">${interview.locationTypeSalary}</p>
                     <br>
 
-                    <p class=" job-status bg-gray-200 font-medium py-2 px-4 w-35 rounded-sm ">NOT APPLIED</p>
-                    <p class="job-details">Build cross-platform mobile applications using React Native. Work on products
-                        used by millions of
-                        users worldwide.</p>
+                   <p class=" btn-interview  py-2 px-4 w-30 rounded-sm font-bold  text-[#10B981] border-2 border-[#10B981]">INTERVIEW</p>
+                    <p class="job-details">${interview.jobDetails}</p>
                     <div class="flex gap-4">
                         <button
                             class=" btn-interview  py-2 px-4 w-30 rounded-sm font-bold  text-[#10B981] border-2 border-[#10B981]">INTERVIEW</button>
@@ -95,6 +122,7 @@ function renderInterview() {
                     </div>
                 </div>
                 `
+        filteredJobList.appendChild(div);
     }
 }
 
