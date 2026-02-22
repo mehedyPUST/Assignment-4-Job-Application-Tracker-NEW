f56ea18af352a6a61977794190632ebef2591387
mehedyPUST <mehedy.pust@gmail.com>
2026-02-22 16:59:51 +0600

    card div created  inner html added

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let rejectedCount = document.getElementById('rejected-count');
 
 const allJobsCard = document.getElementById('all-available-jobs');
 const mainContainer = document.querySelector('main');
+const filteredJobList = document.getElementById('filtered-job-list');
 // console.log(mainContainer);
 
 function calculateCount() {
@@ -44,6 +45,56 @@ function toggleBtnStyle(id) {
 }
 
 mainContainer.addEventListener('click', function (event) {
+    const parentNode = event.target.parentNode.parentNode;
+    const jobProvider = parentNode.querySelector('.job-provider');
+    const jobTitle = parentNode.querySelector('.job-title').innerText;
+    const locationTypeSalary = parentNode.querySelector('.location-type-salary').innerText;
+    const jobStatus = parentNode.querySelector('.job-status').innerText;
+    const jobDetails = parentNode.querySelector('.job-details').innerText;
+
+
+    const cardInfo = {
+        jobProvider,
+        jobTitle,
+        locationTypeSalary,
+        jobStatus,
+        jobDetails
+    };
+
+    const jobProviderExist = interviewList.find(item => item.jobProvider == cardInfo.jobProvider);
+    if (!jobProviderExist) {
+        interviewList.push(cardInfo);
+    }
+    console.log(interviewList);
 
 })
+
+function renderInterview() {
+    filteredJobList.innerHTML = '';
+    for (let interview of interviewList) {
+        let div = document.createElement('div');
+        div.className = 'flex justify-between p-4 border border-blue-200 bg-gray-50 rounded-sm';
+        div.innerHTML = `
+        <div class="space-y-2">
+                    <h2 class="job-provider text-[18px] font-semibold">Mobile First Corp</h2>
+                    <p class="job-title text-[#64748B]">React Native Developer</p>
+                    <br>
+                    <p class="location-type-salary text-[#64748B]">Remote • Full-time • $130,000 - $175,000</p>
+                    <br>
+
+                    <p class=" job-status bg-gray-200 font-medium py-2 px-4 w-35 rounded-sm ">NOT APPLIED</p>
+                    <p class="job-details">Build cross-platform mobile applications using React Native. Work on products
+                        used by millions of
+                        users worldwide.</p>
+                    <div class="flex gap-4">
+                        <button
+                            class=" btn-interview  py-2 px-4 w-30 rounded-sm font-bold  text-[#10B981] border-2 border-[#10B981]">INTERVIEW</button>
+
+                        <button
+                            class=" btn-rejected  py-2 px-4 w-30 rounded-sm  font-bold text-[#EF4444] border-2 border-[#EF4444]">REJECTED</button>
+                    </div>
+                </div>
+                `
+    }
+}
 
